d9e10464cc5d4461140998813835d3993a144855
Thomas Klein <thomas.klein@mpbio.com>
2019-12-10 12:45:34 +0100

    Remove EE eav attributes

--- scripts/attributes.sql
+++ scripts/attributes.sql
@@ -2,4 +2,4 @@
 
 DELETE
 FROM `eav_attribute`
-WHERE `attribute_code` IN ('giftcard_type', 'giftcard_amounts', 'allow_open_amount');
+WHERE `attribute_code` IN ('giftcard_type','giftcard_amounts','allow_open_amount','related_tgtr_position_limit', 'related_tgtr_position_behavior', 'upsell_tgtr_position_limit', 'upsell_tgtr_position_behavior');
